54973ce553624f090ed0ebaba5ae97cfe92fad80
Sumir Vats <sumirvats@gmail.com>
2024-01-04 12:38:52 +0530

    Set database schema and API routes

---
+++ user-manager/server/controller/controller.js
@@ -0,0 +1,15 @@
+import user from "../model/model.js";
+
+// create and save new user
+const create = (req, res) => {};
+
+// retrieve and return all users/single user
+const find = (req, res) => {};
+
+// update user by user id
+const update = (req, res) => {};
+
+// delete user by user id
+const del = (req, res) => {};
+
+export { create, find, update, del };
--- user-manager/server/routes/routes.js
+++ user-manager/server/routes/routes.js
@@ -1,11 +1,18 @@
 import express from "express";
 import "../services/render.js";
 import { addUser, homeRoute, updateUser } from "../services/render.js";
+import { create, find, update, del } from "../controller/controller.js";
 
 const router = express.Router();
 
 router.get("/", homeRoute);
 router.get("/add-user", addUser);
 router.get("/update-user", updateUser);
+
+// API
+router.post("/api/users", create);
+router.get("/api/users", find);
+router.put("/api/users/:id", update);
+router.delete("/api/users/:id", del);
 
 export default router;
